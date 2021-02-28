82fa41511bf2f2c98216ee700dd6baee05a04328
jxc1259 <jxc1259@cs.bham.ac.uk>
2021-02-28 19:34:37 +0800

    Small adjustments

--- src/main/java/com/bham/bc/utils/Constants.java
+++ src/main/java/com/bham/bc/utils/Constants.java
@@ -9,7 +9,7 @@ final public class Constants {
 //    final public static int WINDOW_HEIGHT = 38*TILE_HEIGHT;
     final public static int WINDOW_WIDTH = 800;
     final public static int WINDOW_HEIGHT = 600;
-    final public static int MAP_WIDTH = 1600;
-    final public static int MAP_HEIGHT = 1200;
+    final public static int MAP_WIDTH = 100*TILE_WIDTH;
+    final public static int MAP_HEIGHT = 75*TILE_HEIGHT;
     final public static int FRAME_RATE = 200;
 }
