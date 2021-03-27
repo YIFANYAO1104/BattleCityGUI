0e53da99231517969dd74c7617e3999272c4f678
Yifan Yao <yxy977@cs.bham.ac.uk>
2021-03-27 21:35:06 +0800

    custom slider

--- src/main/java/com/bham/bc/view/CustomStage.java
+++ src/main/java/com/bham/bc/view/CustomStage.java
@@ -52,6 +52,10 @@ public class CustomStage extends Stage{
     public static Label setMenu;
     public static HBox gpTitle;
     public static String[] types;
+    private HBox hBox;
+    private ChoiceBox changeSkin;
+    private Label btnMin;
+    private Label btnClose;
 
 
 
@@ -96,8 +100,8 @@ public class CustomStage extends Stage{
 
     }
 
+    public void titleBar(AnchorPane root, int Width, int Height,int offset){
 
-    public void createCommonTitlebar(AnchorPane root, int Width, int Height){
         stage.initStyle(StageStyle.TRANSPARENT);
 
         gpTitle = new HBox();
@@ -119,7 +123,7 @@ public class CustomStage extends Stage{
         gpTitle.getStylesheets().add(CustomStage.class.getResource("../../../../GUIResources/Stage.css").toExternalForm());
 
 
-        Label btnMin = new Label();
+        btnMin = new Label();
         btnMin.setPrefWidth(33);
         btnMin.setPrefHeight(26);
 
@@ -127,13 +131,13 @@ public class CustomStage extends Stage{
 
         btnMin.setBackground(new Background(image2));
 
-        Label btnClose = new Label();
+        btnClose = new Label();
         btnClose.setPrefWidth(33);
         btnClose.setPrefHeight(26);
 
 
         types=new String[]{"TYPE 1","TYPE 2","TYPE 3","TYPE 4","TYPE 5"};
-        ChoiceBox changeSkin=new ChoiceBox(FXCollections.observableArrayList(
+        changeSkin=new ChoiceBox(FXCollections.observableArrayList(
                 "Classic Black","Classic Grey","Classic Blue","Classic Orange","Classic Gold"
         ));
         changeSkin.setId("changeSkin");
@@ -149,38 +153,43 @@ public class CustomStage extends Stage{
                 BackgroundImage image4=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar05.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
 
                 gpTitle.setBackground(new Background(image4));
-
+                lbTitle.requestFocus();;
 
             }else if (typeOf.equals("TYPE 2")){
                 BackgroundImage image4=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar02.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
 
                 gpTitle.setBackground(new Background(image4));
-
+                lbTitle.requestFocus();;
 
             }else if (typeOf.equals("TYPE 3")){
                 BackgroundImage image4=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
 
                 gpTitle.setBackground(new Background(image4));
-
+                lbTitle.requestFocus();;
 
             }else if (typeOf.equals("TYPE 4")){
                 BackgroundImage image4=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar04.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
 
                 gpTitle.setBackground(new Background(image4));
-
+                lbTitle.requestFocus();;
 
             }else if (typeOf.equals("TYPE 5")){
                 BackgroundImage image4=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar03.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
 
                 gpTitle.setBackground(new Background(image4));
-
+                lbTitle.requestFocus();;
 
             }
 
 
 
         });
-
+        changeSkin.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                lbTitle.requestFocus();;
+            }
+        });
 
 
 
@@ -190,14 +199,18 @@ public class CustomStage extends Stage{
         btnClose.setId("winClose");
         btnMin.setId("winMin");
         //-540 -760
-        lbTitle.setTranslateX(-785);
+        lbTitle.setTranslateX(offset);
         btnMin.setTranslateY(6);
         btnMin.setTranslateX(3);
 
 
 
+        if (setMenu==null){
+            hBox=new HBox(5,changeSkin,btnMin,btnClose);
+        }else {
+            hBox=new HBox(5,setMenu,changeSkin,btnMin,btnClose);
+        }
 
-        HBox hBox=new HBox(5,changeSkin,btnMin,btnClose);
         gpTitle.getChildren().addAll(lbTitle,hBox);
 
         root.getChildren().add(gpTitle);
@@ -344,255 +357,17 @@ public class CustomStage extends Stage{
 
     }
 
+
+    public void createCommonTitlebar(AnchorPane root, int Width, int Height){
+        titleBar(root,Width,Height,-780);
+
+
+    }
+
     public void createTitleBar(AnchorPane root, int Width, int Height){
         startMenuIcon();
-        stage.initStyle(StageStyle.TRANSPARENT);
+        titleBar(root,Width,Height,-540);
 
-        gpTitle = new HBox();
-        gpTitle.setId("title");
-        gpTitle.setSpacing(4);
-        gpTitle.setPadding(new Insets(15,5,17,5));
-        Label lbTitle = new Label("Battle City");
-        lbTitle.setId("name");
-        Glow glow=new Glow();
-        lbTitle.setEffect(glow);
-        glow.setLevel(0.6);
-        BackgroundImage image=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar03.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
-
-        gpTitle.setBackground(new Background(image));
-        gpTitle.setMinWidth(Width);
-        gpTitle.setMaxWidth(Height);
-        gpTitle.setMinHeight(15);
-        gpTitle.setMaxHeight(35);
-        gpTitle.getStylesheets().add(CustomStage.class.getResource("../../../../GUIResources/Stage.css").toExternalForm());
-
-
-        Label btnMin = new Label();
-        btnMin.setPrefWidth(33);
-        btnMin.setPrefHeight(26);
-
-        BackgroundImage image2=new BackgroundImage(new Image("file:src/main/resources/GUIResources/Min.png",24,10,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
-
-        btnMin.setBackground(new Background(image2));
-
-        Label btnClose = new Label();
-        btnClose.setPrefWidth(33);
-        btnClose.setPrefHeight(26);
-
-
-        types=new String[]{"TYPE 1","TYPE 2","TYPE 3","TYPE 4","TYPE 5"};
-        ChoiceBox changeSkin=new ChoiceBox(FXCollections.observableArrayList(
-                "Classic Black","Classic Grey","Classic Blue","Classic Orange","Classic Gold"
-        ));
-        changeSkin.setId("changeSkin");
-        changeSkin.setMaxSize(25,22);
-        changeSkin.setMinSize(25,22);
-        BackgroundImage image3=new BackgroundImage(new Image("file:src/main/resources/GUIResources/skinSet.png",25,22,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
-        changeSkin.setBackground(new Background(image3));
-        changeSkin.getSelectionModel().selectedIndexProperty().addListener((ObservableValue<? extends Number> ov,Number old_val,Number new_val)->{
-
-
-            typeOf = types[new_val.intValue()];
-            if (typeOf.equals("TYPE 1")){
-                BackgroundImage image4=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar05.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
-
-                gpTitle.setBackground(new Background(image4));
-                setMenu.requestFocus();
-
-            }else if (typeOf.equals("TYPE 2")){
-                BackgroundImage image4=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar02.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
-
-                gpTitle.setBackground(new Background(image4));
-                setMenu.requestFocus();
-
-            }else if (typeOf.equals("TYPE 3")){
-                BackgroundImage image4=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
-
-                gpTitle.setBackground(new Background(image4));
-                setMenu.requestFocus();
-
-            }else if (typeOf.equals("TYPE 4")){
-                BackgroundImage image4=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar04.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
-
-                gpTitle.setBackground(new Background(image4));
-                setMenu.requestFocus();
-
-            }else if (typeOf.equals("TYPE 5")){
-                BackgroundImage image4=new BackgroundImage(new Image("file:src/main/resources/GUIResources/menuBar03.png",Width,34,false,true), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
-
-                gpTitle.setBackground(new Background(image4));
-                setMenu.requestFocus();
-
-            }
-
-
-
-        });
-        changeSkin.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                setMenu.requestFocus();
-            }
-        });
-
-
-
-
-
-
-        btnClose.setId("winClose");
-        btnMin.setId("winMin");
-        //-540 -760
-        lbTitle.setTranslateX(-540);
-        btnMin.setTranslateY(6);
-        btnMin.setTranslateX(3);
-
-
-
-
-        HBox hBox=new HBox(5,setMenu,changeSkin,btnMin,btnClose);
-        gpTitle.getChildren().addAll(lbTitle,hBox);
-
-        root.getChildren().add(gpTitle);
-
-        gpTitle.setLayoutX(0);
-        gpTitle.setLayoutY(0);
-        gpTitle.setAlignment(Pos.CENTER_RIGHT);
-
-
-
-        btnMin.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                stage.setIconified(true);
-            }
-
-
-
-        });
-
-        btnClose.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                stage.close();
-            }
-        });
-
-        stage.xProperty().addListener(new ChangeListener<Number>() {
-
-
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if (newValue != null && !isMax) {
-                    x = newValue.doubleValue();
-                }
-            }
-        });
-        stage.yProperty().addListener(new ChangeListener<Number>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if (newValue != null && !isMax) {
-                    y = newValue.doubleValue();
-                }
-            }
-        });
-        stage.widthProperty().addListener(new ChangeListener<Number>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if (newValue != null && !isMax) {
-                    width = newValue.doubleValue();
-                }
-            }
-        });
-        stage.heightProperty().addListener(new ChangeListener<Number>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if (newValue != null && !isMax) {
-                    height = newValue.doubleValue();
-                }
-            }
-        });
-
-        root.setOnMouseMoved((MouseEvent event) -> {
-            event.consume();
-            double x = event.getSceneX();
-            double y = event.getSceneY();
-            double width = stage.getWidth();
-            double height = stage.getHeight();
-            Cursor cursorType = Cursor.DEFAULT;// 鼠标光标初始为默认类型，若未进入调整窗口状态，保持默认类型
-            // 先将所有调整窗口状态重置
-            isRight = isBottomRight = isBottom = false;
-            if (y >= height - RESIZE_WIDTH) {
-                if (x <= RESIZE_WIDTH) {// 左下角调整窗口状态
-                    //不处理
-
-                } else if (x >= width - RESIZE_WIDTH) {// 右下角调整窗口状态
-                    isBottomRight = true;
-                    cursorType = Cursor.SE_RESIZE;
-                } else {// 下边界调整窗口状态
-                    isBottom = true;
-                    cursorType = Cursor.S_RESIZE;
-                }
-            } else if (x >= width - RESIZE_WIDTH) {// 右边界调整窗口状态
-                isRight = true;
-                cursorType = Cursor.E_RESIZE;
-            }
-            // 最后改变鼠标光标
-            root.setCursor(cursorType);
-        });
-
-        root.setOnMouseDragged((MouseEvent event) -> {
-
-            //根据鼠标的横纵坐标移动dialog位置
-            event.consume();
-            if (yOffset != 0 ) {
-                stage.setX(event.getScreenX() - xOffset);
-                if (event.getScreenY() - yOffset < 0) {
-                    stage.setY(0);
-                } else {
-                    stage.setY(event.getScreenY() - yOffset);
-                }
-            }
-
-            double x = event.getSceneX();
-            double y = event.getSceneY();
-            // 保存窗口改变后的x、y坐标和宽度、高度，用于预判是否会小于最小宽度、最小高度
-            double nextX = stage.getX();
-            double nextY = stage.getY();
-            double nextWidth = stage.getWidth();
-            double nextHeight = stage.getHeight();
-            if (isRight || isBottomRight) {// 所有右边调整窗口状态
-                nextWidth = x;
-            }
-            if (isBottomRight || isBottom) {// 所有下边调整窗口状态
-                nextHeight = y;
-            }
-            if (nextWidth <= MIN_WIDTH) {// 如果窗口改变后的宽度小于最小宽度，则宽度调整到最小宽度
-                nextWidth = MIN_WIDTH;
-            }
-            if (nextHeight <= MIN_HEIGHT) {// 如果窗口改变后的高度小于最小高度，则高度调整到最小高度
-                nextHeight = MIN_HEIGHT;
-            }
-            // 最后统一改变窗口的x、y坐标和宽度、高度，可以防止刷新频繁出现的屏闪情况
-            stage.setX(nextX);
-            stage.setY(nextY);
-            stage.setWidth(nextWidth);
-            stage.setHeight(nextHeight);
-
-        });
-        //鼠标点击获取横纵坐标
-        root.setOnMousePressed(event -> {
-            event.consume();
-            xOffset = event.getSceneX();
-            if (event.getSceneY() > 46) {
-                yOffset = 0;
-            } else {
-                yOffset = event.getSceneY();
-            }
-        });
 
 
 
