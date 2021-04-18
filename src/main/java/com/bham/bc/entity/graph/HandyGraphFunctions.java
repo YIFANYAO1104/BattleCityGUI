6fdd2d6ec99b1c19465c6d9705a5cea2c8f0ff97
fivn869 <yanxiao5211@gmail.com>
2021-04-19 00:46:40 +0800

    add sysout function to show

--- src/main/java/com/bham/bc/entity/graph/HandyGraphFunctions.java
+++ src/main/java/com/bham/bc/entity/graph/HandyGraphFunctions.java
@@ -109,6 +109,7 @@ public class HandyGraphFunctions {
                 double total = NumCellsX * NumCellsY;
                 double realPer  = realTimeNodes / (total*2);
                 graph.setRealContrustPercentage(realPer);
+                System.out.println("grph comstruct:  "+graph.getRealContrustPercentage());
 
             }
         }
@@ -122,7 +123,7 @@ public class HandyGraphFunctions {
                 double total = NumCellsX * NumCellsY;
                 double realPer = 1.0 / (total*2) + graph.getRealContrustPercentage();
                 graph.setRealContrustPercentage(realPer);
-//                System.out.println(graph.getRealContrustPercentage());
+                System.out.println("grph comstruct:  "+ graph.getRealContrustPercentage());
             }
         }
     }
